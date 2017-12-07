3f114ee1106b7ff6d41641dd59080cd902a9b630
Takuya Mukohira <takuya.mk96@gmail.com>
2017-12-07 11:42:56 +0900

    Update

--- Day03-01/Form1.Designer.vb
+++ Day03-01/Form1.Designer.vb
@@ -34,7 +34,14 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Button7 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -59,7 +66,7 @@ Partial Class Form1
         '
         Me.PictureBox1.Location = New System.Drawing.Point(152, 12)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(420, 360)
+        Me.PictureBox1.Size = New System.Drawing.Size(360, 360)
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
@@ -69,21 +76,21 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(46, 278)
+        Me.TextBox1.Location = New System.Drawing.Point(46, 454)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 19)
         Me.TextBox1.TabIndex = 3
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(46, 303)
+        Me.TextBox2.Location = New System.Drawing.Point(46, 479)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(100, 19)
         Me.TextBox2.TabIndex = 4
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(46, 328)
+        Me.TextBox3.Location = New System.Drawing.Point(46, 504)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(100, 19)
         Me.TextBox3.TabIndex = 5
@@ -91,7 +98,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(15, 281)
+        Me.Label1.Location = New System.Drawing.Point(15, 457)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(25, 12)
         Me.Label1.TabIndex = 6
@@ -100,7 +107,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(5, 306)
+        Me.Label2.Location = New System.Drawing.Point(5, 482)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(35, 12)
         Me.Label2.TabIndex = 7
@@ -109,7 +116,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 331)
+        Me.Label3.Location = New System.Drawing.Point(12, 507)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(28, 12)
         Me.Label3.TabIndex = 8
@@ -117,7 +124,7 @@ Partial Class Form1
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(46, 353)
+        Me.TextBox4.Location = New System.Drawing.Point(46, 529)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(100, 19)
         Me.TextBox4.TabIndex = 9
@@ -125,17 +132,76 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(7, 356)
+        Me.Label4.Location = New System.Drawing.Point(7, 532)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(33, 12)
         Me.Label4.TabIndex = 10
         Me.Label4.Text = "(X, Y)"
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(560, 527)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "色チェック"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(641, 527)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "色反転"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(722, 527)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "左右反転"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(803, 527)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 14
+        Me.Button6.Text = "上下反転"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(518, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(360, 360)
+        Me.PictureBox2.TabIndex = 15
+        Me.PictureBox2.TabStop = False
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(71, 425)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 16
+        Me.Button7.Text = "Reｄ"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(584, 442)
+        Me.ClientSize = New System.Drawing.Size(884, 562)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.Label3)
@@ -150,6 +216,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -166,5 +233,11 @@ Partial Class Form1
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
     Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
 
 End Class
--- Day03-01/Form1.vb
+++ Day03-01/Form1.vb
@@ -1,8 +1,12 @@
 ﻿Public Class Form1
+    Dim mode As Integer = 1
+    Dim i As Integer
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         OpenFileDialog1.ShowDialog()
         PictureBox1.Image = System.Drawing.Image.FromFile(OpenFileDialog1.FileName)
+        PictureBox2.Width = PictureBox1.Width
+        PictureBox2.Height = PictureBox1.Height
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -27,5 +31,85 @@
 
     Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
         PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        mode = 1
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim col As Color
+        Dim x, y As Integer
+        Dim bm As New Bitmap(PictureBox1.Image)
+        Dim g As Graphics = PictureBox2.CreateGraphics()
+        Dim p As New Pen(Color.Black)
+
+        For x = 0 To PictureBox1.Width - 1
+            For y = 0 To PictureBox1.Height - 1
+                col = bm.GetPixel(x, y)
+                col = Color.FromArgb(255 - col.R, 255 - col.G, 255 - col.B)
+
+                p.Color = col
+                g.DrawEllipse(p, x, y, 1, 1)
+            Next
+        Next
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim col As Color
+        Dim x, y As Integer
+        Dim bm As New Bitmap(PictureBox1.Image)
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim p As New Pen(Color.Black)
+
+        bm.RotateFlip(RotateFlipType.Rotate180FlipY)
+        PictureBox1.Image = bm
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim col As Color
+        Dim x, y As Integer
+        Dim bm As New Bitmap(PictureBox1.Image)
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim p As New Pen(Color.Black)
+
+        bm.RotateFlip(RotateFlipType.Rotate180FlipX)
+        PictureBox1.Image = bm
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        i = 0
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim col As Color
+        Dim x, y As Integer
+        Dim bm As New Bitmap(PictureBox1.Image)
+        Dim g As Graphics = PictureBox2.CreateGraphics()
+        Dim p As New Pen(Color.Black)
+
+        i += 1
+        If i > 2 Then
+            i = 0
+        End If
+        Button7.Text = i
+
+        For x = 0 To PictureBox1.Width - 1
+            For y = 0 To PictureBox1.Height - 1
+                col = bm.GetPixel(x, y)
+
+                Select Case i
+                    Case 0
+                        col = Color.FromArgb(col.R, col.G, col.B)
+                    Case 1
+                        col = Color.FromArgb(col.B, col.R, col.G)
+                    Case 2
+                        col = Color.FromArgb(col.G, col.B, col.R)
+                End Select
+
+                p.Color = col
+                g.DrawEllipse(p, x, y, 1, 1)
+            Next
+        Next
     End Sub
 End Class
